37ac0d518265c856acbb91028f5298af17a5d580
小吉雄也 <y.koyoshi.5.22@gmail.com>
2018-12-14 21:26:48 +0900

    fix:閲覧機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,15 @@ class PostsController < ApplicationController
   def create
   	post = Post.new(post_params)
   	post.save
-  	redirect_to '/top'
+  	redirect_to post_path(post.id)
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id])
   end
 
   private
